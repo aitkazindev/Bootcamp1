5f2195fe8ce259bb9abdbefed4cabaa2a1206f90
Ibrahim <aitkazindev@gmail.com>
2024-10-02 21:35:03 +0900

    Initial Commit

---
+++ Bootcamp1/Bootcamp1App.swift
@@ -0,0 +1,17 @@
+//
+//  Bootcamp1App.swift
+//  Bootcamp1
+//
+//  Created by Ibrahim Aitkazin on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bootcamp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
