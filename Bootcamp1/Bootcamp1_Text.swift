8f7213766c6a481c28c2caaa04a02e806f1aa3ce
Ibrahim <aitkazindev@gmail.com>
2024-10-02 22:21:28 +0900

    Text

--- Bootcamp1/Bootcamp1_Text.swift
+++ Bootcamp1/Bootcamp1_Text.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct Bootcamp1_Text: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
+        
+            //.font(.body)
+            //.fontWeight(.semibold)
+            //.underline()
+            //.underline(true,color: Color.red)
+            //.italic()
+            //.strikethrough()
+            //.strikethrough(true, color: Color.green)
+            //.font(.system(size: 24, weight: .semibold, design: .serif))
+            //.baselineOffset(20.0)
+            //.kerning(10.0)
+            .multilineTextAlignment(.leading)
+            .foregroundStyle(.blue)
+            .frame(width: 200,height: 100,alignment: .leading)
+            .minimumScaleFactor(0.1)
+            
     }
 }
 
