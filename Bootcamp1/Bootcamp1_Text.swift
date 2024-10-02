4a0500d88e3ba6075566c2fc739996ad11c19567
Ibrahim <aitkazindev@gmail.com>
2024-10-02 22:14:40 +0900

    Day 1: Text and properties

---
+++ Bootcamp1/Bootcamp1_Text.swift
@@ -0,0 +1,18 @@
+//
+//  Bootcamp1_Text.swift
+//  Bootcamp1
+//
+//  Created by Ibrahim Aitkazin on 02.10.2024.
+//
+
+import SwiftUI
+
+struct Bootcamp1_Text: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bootcamp1_Text()
+}
