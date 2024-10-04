8c933bcc2c05b1376b84a65a1c2ca9f3410bb35f
Ibrahim <aitkazindev@gmail.com>
2024-10-05 00:19:24 +0900

    - Color

--- Bootcamp1/Bootcamp3_Colors.swift
+++ Bootcamp1/Bootcamp3_Colors.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 struct Bootcamp3_Colors: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25.0)
+            //.fill(Color.red)
+            .fill(
+                //Color(Color.primary)
+                //Color(Color.secondary)
+                //Color(uiColor: UIColor.secondarySystemBackground)
+                Color("CustomColor", bundle: .main)
+            )
+            .frame(width: 300,height: 200)
+            //.shadow(radius: 10)
+            .shadow(color: Color("CustomColor", bundle: .main).opacity(0.3), radius: 10, x: -20,y: -20)
+            
     }
 }
 
 #Preview {
     Bootcamp3_Colors()
+        //.preferredColorScheme(.dark)
 }
