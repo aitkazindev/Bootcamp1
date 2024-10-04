26e2fd8acd128c0ca5342a41feeb292b434d5129
Ibrahim <aitkazindev@gmail.com>
2024-10-05 00:18:52 +0900

    - Color

---
+++ Bootcamp1/Bootcamp3_Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Bootcamp3_Colors.swift
+//  Bootcamp1
+//
+//  Created by Ibrahim Aitkazin on 04.10.2024.
+//
+
+import SwiftUI
+
+struct Bootcamp3_Colors: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Bootcamp3_Colors()
+}
